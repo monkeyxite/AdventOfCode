9e795dcb21b956016c1d751440f9f35cc7df8676
jonny houy <jonny.gou@gmail.com>
2021-12-12 12:47:28 +0100

    2nd part trial

--- 2021/1/solver.py
+++ 2021/1/solver.py
@@ -1,12 +1,20 @@
-def main():
-    with open("test.txt") as f:
+def main(fn):
+    with open(fn) as f:
         l = f.read().split("\n")
-        l = [int(i) for i in l]
+        print(l)
+        l = [int(i) for i in l if i]
+    l2 = [sum(l[i:i+2]) for i in range(len(l)-2)]
+    return cmp(l),cmp(l2)
+
+
+
+def cmp(l):
     cnt = 0
     for i in range(len(l) - 1):
         cnt = cnt + 1 if l[i + 1] > l[i] else cnt
     return cnt
 
-
 if __name__ == "__main__":
-    main()
+    fn = input("pls fill in filename to analysis:\n")
+    result = main(fn)
+    print(f"the results is {result}")
