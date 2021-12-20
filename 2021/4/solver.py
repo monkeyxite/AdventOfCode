8b00879918f5dcf3f93a7c45f4acc281f8c0e3e1
Jonny Hou <monkeyxite@gmail.com>
2021-12-20 10:57:56 +0100

    Done D4 P1

--- 2021/4/solver.py
+++ 2021/4/solver.py
@@ -13,26 +13,49 @@ def main(fn):
     for i in num:
         for b in bl:
             b.match(i)
-            pprint(b.m)
-            b.win()
-    return num
+            if b.win(wl):
+                pprint(f"{b} wins @ {i}")
+                result = (
+                    i * sum(set(b.num) - set(num[:num.index(i) + 1])),
+                    f" {i} * {sum(set(b.num)-set(num[:num.index(i)+1]))}",
+                )
+                break
+        else:
+            continue
+        break
+    return result
 
 
 class Board:
     """docstring for Board."""
-
     def __init__(self, num_list):
         self.num = [int(i) for i in num_list]
-        self.m = []
+        self.m = set()
 
     def __repr__(self):
         return str(self.num)
 
     def match(self, n):
-        self.m.append(self.num.index(n))
+        try:
+            self.m.add(self.num.index(n))
+        except ValueError:
+            print(f"not found {n} in {self}")
 
-    def win(self):
-        return False
+    def win(self, wl):
+        wl = wl
+        result = list(filter(lambda i: i.issubset(self.m), wl))
+        return True if result else False
+
+
+def winlist_gen():
+    win_list = []
+    for g in range(5):
+        win_list.append({i + 5 * g for i in range(5)})
+        win_list.append({i + g for i in range(0, 25, 5)})
+    return win_list
+
+
+wl = winlist_gen()
 
 
 def main2(fn):
@@ -42,4 +65,5 @@ def main2(fn):
 if __name__ == "__main__":
     fn = input("pls fill in filename to analysis:\n")
     result = main(fn)
+    print(f"part1 the reuslt is {result}; ")
     result2 = main2(fn)
