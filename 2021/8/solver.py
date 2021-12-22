bb58c231fd67d20c0ba548d5bbf7b44b75881a1c
Jonny Hou <monkeyxite@gmail.com>
2021-12-22 22:16:51 +0100

    D8 p2 trial :P

--- 2021/8/solver.py
+++ 2021/8/solver.py
@@ -10,8 +10,40 @@ def main(s):
     return counter
 
 
-def main2():
-    pass
+def main2(s, cb):
+    samples = s
+    codebook = cb
+    result = []
+    for i in samples:
+        for q in codebook.keys():
+            if set(i) == set(q):
+                result.append(codebook[q])
+            else:
+                print(f"{i} doesn't match the codebook")
+    print(result, len(result))
+    comb = [
+        1000 * result[i] + 100 * result[i + 1] + 10 * result[i + 2] +
+        result[i + 3] for i in range(0, len(result), 4)
+    ]
+    print(comb)
+    return sum(comb)
+
+
+def decoding(sources):
+    postion_holder = ["", "", "", "", "", "", ""]
+    c7 = set(list(filter(lambda i: len(i) == 3, sources))[0])
+    c1 = set(list(filter(lambda i: len(i) == 2, sources))[0])
+    c8 = set(list(filter(lambda i: len(i) == 7, sources))[0])
+    c4 = set(list(filter(lambda i: len(i) == 4, sources))[0])
+    c253 = list(map(set, list(filter(lambda i: len(i) == 5, sources))))
+    c3 = list(filter(lambda i: len(i&c1)==2, c253))[0]
+    c069 = list(map(set, list(filter(lambda i: len(i) == 6, sources)))
+    c6 = list(filter(lambda i: len(i&c1)==1, c069))[0]
+    postion_holder[6] = c7 - c1
+    postion_holder[2] = c6 & c1
+    postion_holder[1] = c1-(c6 & c1)
+    postion_holder[6] = c1-(c6 & c1)
+    return postion_holder
 
 
 if __name__ == "__main__":
@@ -19,25 +51,29 @@ if __name__ == "__main__":
     parser.add_argument("fn", nargs="?", default="test.txt")
     fn = parser.parse_args().fn
 
+    postion_holder = ["", "", "", "", "", "", ""]
     codebook = {
-        0: "abcdfg",
-        1: "cf",
-        2: "acdeg",
-        3: "acdfg",
-        4: "bcdf",
-        5: "abdfg",
-        6: "abdefg",
-        7: "acf",
-        8: "abcdefg",
-        9: "abcdfg",
+        8: (1, 2, 3, 4, 5, 6, 0),
+        5: (2, 3, 5, 6, 0),
+        2: (1, 3, 4, 6, 0),
+        3: (1, 2, 3, 6, 0),
+        7: (6, 1, 2),
+        9: (1, 2, 3, 5, 6, 0),
+        6: (2, 3, 4, 5, 6, 0),
+        4: (1, 2, 5, 0),
+        0: (1, 2, 3, 4, 5, 6),
+        1: (1, 2),
     }
-
     with open(fn) as f:
         lines = f.read().strip().split("\n")
 
     samples = []
+    sources = []
     for line in lines:
         samples.extend(line.split("|")[1].split())
+        sources.extend(line.split("|")[0].split())
+
     print(samples)
     result = main(samples)
-    print(f"the results is: \n  part 1 {result}; part 2 {result}")
+    result2 = main2(samples, codebook)
+    print(f"the results is: \n  part 1 {result}; part 2 {result2}")
