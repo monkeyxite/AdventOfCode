bf9bb7031e2a1ac912986392a20039ea97597aef
Jonny Hou <monkeyxite@gmail.com>
2021-12-27 15:12:39 +0100

    d13 p1 trial

--- 2021/13/solver.py
+++ 2021/13/solver.py
@@ -2,8 +2,21 @@ import argparse
 from collections import defaultdict
 
 
-def main():
-    return 0
+def main(pts, folder):
+    x, y = zip(*pts)
+    ax, where = folder
+    boundary = max(x) if ax == "x" else max(y)
+    if ax == "x":
+        new_x = [2 * where - i for i in x]
+        new_pts = zip(new_x, y)
+        remain_pts = [pt for pt in pts if pt[0] < where]
+    else:
+        new_y = [2 * where - i for i in y]
+        new_pts = zip(x, new_y)
+        remain_pts = [pt for pt in pts if pt[1] < where]
+        print(remain_pts)
+        print(new_pts)
+    return len(set(remain_pts) & set(new_pts))
 
 
 def main2():
@@ -18,12 +31,19 @@ if __name__ == "__main__":
     with open(fn) as f:
         matrix, actions = f.read().strip().split("\n\n")
 
+    print(matrix)
     pts = []
-    for pt in matrix:
-        xy = pt.split(", ")
-        pts.append(xy)
+    f = []
+    for pt in matrix.splitlines():
+        x, y = pt.split(",")
+        pts.append((int(x), int(y)))
 
-    print(pts)
-    result = main()
+    for a in actions.splitlines():
+        axis, num = a.replace("fold along ", "").split("=")
+        f.append((axis, int(num)))
+        # pts.append(xy)
+
+    print(f)
+    result = main(pts, f[0])
     result2 = main2()
     print(f"the results is: \n  part 1 {result}; part 2 {result2}")
