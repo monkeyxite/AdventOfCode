5d5b2b96d4265999231f92fb2f25b8507841fdaf
Jonny Hou <monkeyxite@gmail.com>
2021-12-27 18:08:13 +0100

    D13 P1 done :)

--- 2021/13/solver.py
+++ 2021/13/solver.py
@@ -3,20 +3,16 @@ from collections import defaultdict
 
 
 def main(pts, folder):
-    x, y = zip(*pts)
+    # x, y = zip(*pts)
     ax, where = folder
-    boundary = max(x) if ax == "x" else max(y)
+    # boundary = max(x) if ax == "x" else max(y)
     if ax == "x":
-        new_x = [2 * where - i for i in x]
-        new_pts = zip(new_x, y)
-        remain_pts = [pt for pt in pts if pt[0] < where]
+        new_pts = set([(2 * where - i[0], i[1]) for i in pts if i[0] > where])
+        remain_pts = set([pt for pt in pts if pt[0] < where])
     else:
-        new_y = [2 * where - i for i in y]
-        new_pts = zip(x, new_y)
-        remain_pts = [pt for pt in pts if pt[1] < where]
-        print(remain_pts)
-        print(new_pts)
-    return len(set(remain_pts) & set(new_pts))
+        new_pts = set([(i[0], 2 * where - i[1]) for i in pts if i[1] > where])
+        remain_pts = set([pt for pt in pts if pt[1] < where])
+    return len(remain_pts | new_pts)
 
 
 def main2():
@@ -31,7 +27,6 @@ if __name__ == "__main__":
     with open(fn) as f:
         matrix, actions = f.read().strip().split("\n\n")
 
-    print(matrix)
     pts = []
     f = []
     for pt in matrix.splitlines():
@@ -43,7 +38,6 @@ if __name__ == "__main__":
         f.append((axis, int(num)))
         # pts.append(xy)
 
-    print(f)
     result = main(pts, f[0])
     result2 = main2()
     print(f"the results is: \n  part 1 {result}; part 2 {result2}")
