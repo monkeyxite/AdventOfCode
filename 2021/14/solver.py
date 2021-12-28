a5f31a2ef0e36af06cc8d6e11d2e5e11c7087527
Jonny Hou <monkeyxite@gmail.com>
2021-12-28 20:58:16 +0100

    Trial D14 P1 ....

--- 2021/14/solver.py
+++ 2021/14/solver.py
@@ -1,9 +1,32 @@
 import argparse
-from collections import defaultdict
+from collections import Counter
 
 
-def main():
-    pass
+def main(init, pts):
+    init_status = []
+    for i in range(len(init) - 1):
+        init_status.append(init[i:i + 2])
+    counter = Counter(init_status)
+
+    # p, addin = zip(*pts)
+    step = 10
+
+    for _ in range(step):
+        new_counter = Counter()
+        for k, v in counter.items():
+            if k in pts.keys():
+                print(f"found {k}")
+                new_counter[k[0] + pts[k]] += v
+                new_counter[pts[k] + k[1]] += v
+            print(counter)
+            print(new_counter)
+        counter = new_counter
+
+    result = Counter()
+    for i in counter.keys():
+        result.update({i[0]: counter[i], i[1]: counter[i]})
+
+    print(sum(result.values()) / 2 + 1)
 
 
 def main2():
@@ -16,19 +39,13 @@ if __name__ == "__main__":
     fn = parser.parse_args().fn
 
     with open(fn) as f:
-        matrix, actions = f.read().strip().split("\n\n")
+        init, pattern = f.read().strip().split("\n\n")
 
-    pts = []
-    f = []
-    for pt in matrix.splitlines():
-        x, y = pt.split(",")
-        pts.append((int(x), int(y)))
+    pts = {}
+    for pt in pattern.splitlines():
+        p, addin = pt.split(" -> ")
+        pts[p] = addin
 
-    for a in actions.splitlines():
-        axis, num = a.replace("fold along ", "").split("=")
-        f.append((axis, int(num)))
-        # pts.append(xy)
-
-    result = main()
+    result = main(init, pts)
     result2 = main2()
     print(f"the results is: \n  part 1 {result}; part 2 {result2}")
