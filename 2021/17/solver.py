0bea65ea3bb6771ade7d56d53c59eae9319b4411
Jonny Hou <monkeyxite@gmail.com>
2022-01-02 18:43:54 +0100

    D17 P1, P2 done :)

--- 2021/17/solver.py
+++ 2021/17/solver.py
@@ -2,33 +2,31 @@ import argparse
 
 
 def main():
-    guess_v_range = ((0, int(x_min)), (int(y_min), -int(y_min)))
+    guess_v_range = ((1, int(x_max) + 1), (int(y_min) - 1, abs(int(y_min))))
     print(guess_v_range)
     result = 0
+    right_v = []
     for i in range(guess_v_range[0][0], guess_v_range[0][1]):
         for q in range(guess_v_range[1][0], guess_v_range[1][1]):
             pos = start
-            v = [i, q]
-            print(f"{v}")
+            init_v = v = (i, q)
             high = 0
+            # print(f"init v: {v}")
             while True:
-                new_pos = (pos[0] + v[0], pos[1] + v[1])
+                pos = (pos[0] + v[0], pos[1] + v[1])
                 v = v_changing(v)
-                high = new_pos[1] if new_pos[1] > high else high
-                # print(f"new high is {high}, {new_pos}")
-                if new_pos[0] in target[0] and new_pos[1] in target[1]:
-                    print(f"hit target via {new_pos} with {high} @ {v}")
+                high = pos[1] if pos[1] > high else high
+                if pos[0] in target[0] and pos[1] in target[1]:
+                    if init_v not in right_v:
+                        right_v.append(init_v)
+                    # print( f"hit target via {pos} with {high} @ {v} with init v {init_v}")
                     result = high if high > result else result
                     break
-                elif new_pos[0] > int(x_max) or new_pos[1] < int(y_min):
+                elif pos[0] > int(x_max) or pos[1] < int(y_min):
                     break
                 else:
                     continue
-    return result
-
-
-def main2():
-    pass
+    return result, right_v
 
 
 def v_changing(v):
@@ -57,10 +55,12 @@ if __name__ == "__main__":
     x_min, x_max = x.replace("x=", "").split("..")
     y_min, y_max = y.replace("y=", "").split("..")
 
-    target = (range(int(x_min), int(x_max)), range(int(y_min), int(y_max)))
+    target = (range(int(x_min),
+                    int(x_max) + 1), range(int(y_min),
+                                           int(y_max) + 1))
     start = [0, 0]
 
     # print(coord, max(coord))
     result = main()
     result2 = main2()
-    print(f"the results is: \n  part 1 {result}; part 2 {result}")
+    print(f"the results is: \n  part 1 {result[0]}; part 2 {len(result[1])}")
