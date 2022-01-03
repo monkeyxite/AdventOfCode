14a5aa0b761e8ddba3a1b15b1c0f3d13720afb21
Jonny Hou <monkeyxite@gmail.com>
2022-01-03 22:54:18 +0100

    D18 P1,P2 done :)

--- 2021/18/solver.py
+++ 2021/18/solver.py
@@ -1,45 +1,80 @@
 import argparse
+from itertools import permutations
+
+
+def add_left(a, b):
+    if not b:
+        return a
+    if type(a) == int:
+        return a + b
+    return [add_left(a[0], b), a[1]]
+
+
+def add_right(a, b):
+    if not b:
+        return a
+    if type(a) == int:
+        return a + b
+    return [a[0], add_right(a[1], b)]
+
+
+def explode_pair(x, layer):
+    if type(x) == int:
+        return False, x, 0, 0
+    left, right = x
+    if layer == 4:
+        return True, 0, left, right
+    exploded, nxt, l, r = explode_pair(left, layer+1)
+    if exploded:
+        return True, [nxt, add_left(right, r)], l, 0
+    exploded, nxt, l, r = explode_pair(right, layer+1)
+    if exploded:
+        return True, [add_right(left, l), nxt], 0, r
+    return False, x, 0, 0
+
+
+def split_num(x):
+    if type(x) == int:
+        if x > 9:
+            return [x // 2, x // 2 + (x & 1)]
+        return x
+    l, r = x
+    left = split_num(l)
+    if left != l:
+        return [left, r]
+    return [l, split_num(r)]
+
+
+def add_snails(a, b):
+    res = [a, b]
+    while True:
+        exploded, res, _, _ = explode_pair(res, 0)
+        if not exploded:
+            prev = res
+            res = split_num(res)
+            if res == prev:
+                return res
+
+
+def magnitude(x):
+    if type(x) == int:
+        return x
+    return 3 * magnitude(x[0]) + 2 * magnitude(x[1])
 
 
 def main():
-    guess_v_range = ((1, int(x_max) + 1), (int(y_min) - 1, abs(int(y_min))))
-    print(guess_v_range)
-    result = 0
-    right_v = []
-    for i in range(guess_v_range[0][0], guess_v_range[0][1]):
-        for q in range(guess_v_range[1][0], guess_v_range[1][1]):
-            pos = start
-            init_v = v = (i, q)
-            high = 0
-            # print(f"init v: {v}")
-            while True:
-                pos = (pos[0] + v[0], pos[1] + v[1])
-                v = v_changing(v)
-                high = pos[1] if pos[1] > high else high
-                if pos[0] in target[0] and pos[1] in target[1]:
-                    if init_v not in right_v:
-                        right_v.append(init_v)
-                    # print( f"hit target via {pos} with {high} @ {v} with init v {init_v}")
-                    result = high if high > result else result
-                    break
-                elif pos[0] > int(x_max) or pos[1] < int(y_min):
-                    break
-                else:
-                    continue
-    return result, right_v
+    cur = DATA[0]
+    for line in DATA[1:]:
+        cur = add_snails(cur, line)
+    return magnitude(cur)
 
 
-def v_changing(v):
-    v_x, v_y = v
-    if v_x > 0:
-        v_x += -1
-    elif v_x < 0:
-        v_x += 1
-    else:
-        v_x = 0
-
-    v_y += -1
-    return (v_x, v_y)
+def main2():
+    res = 0
+    for a, b in permutations(DATA, 2):
+        cur = add_snails(a, b)
+        res = max(res, magnitude(cur))
+    return res
 
 
 if __name__ == "__main__":
@@ -50,17 +85,8 @@ if __name__ == "__main__":
     with open(fn) as f:
         lines = f.read()
 
-    _, Target = lines.split(": ")
-    x, y = Target.split(", ")
-    x_min, x_max = x.replace("x=", "").split("..")
-    y_min, y_max = y.replace("y=", "").split("..")
+    DATA = [eval(line) for line in lines.splitlines()]
 
-    target = (range(int(x_min),
-                    int(x_max) + 1), range(int(y_min),
-                                           int(y_max) + 1))
-    start = [0, 0]
-
-    # print(coord, max(coord))
     result = main()
     result2 = main2()
-    print(f"the results is: \n  part 1 {result[0]}; part 2 {len(result[1])}")
+    print(f"the results is: \n  part 1 {result}; part 2 {result2}")
