7f7224b4047afff9248e280598c359d7e2c007d2
Jonny Hou <monkeyxite@gmail.com>
2022-01-05 21:04:06 +0100

    Bootstrap D20.

---
+++ 2021/20/solver.py
@@ -0,0 +1,24 @@
+import argparse
+from collections import defaultdict
+
+
+def main():
+    return 0
+
+
+def main2():
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fn", nargs="?", default="test.txt")
+    fn = parser.parse_args().fn
+
+    with open(fn) as f:
+        matrix, actions = f.read().strip().split("\n\n")
+
+
+    result = main()
+    result2 = main2()
+    print(f"the results is: \n  part 1 {result}; part 2 {result2}")
